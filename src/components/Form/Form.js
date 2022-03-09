85a2529863f0497d8526a6a4b97f3066ff620e94
programmer-shaheb <abidurrahman471@gmail.com>
2022-03-09 13:10:34 +0600

    header component

---
+++ src/components/Form/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./Form.module.css";
+
+const Form = () => {
+  return (
+    <>
+      <div className={classes.form_layout}>
+        <h1>INQUIRY FORM</h1>
+      </div>
+    </>
+  );
+};
+
+export default Form;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Typography } from "antd";
+import { Divider } from "antd";
+
+import classes from "./Header.module.css";
+
+const { Title } = Typography;
+
+const Header = () => {
+  return (
+    <>
+      <div className={classes.header_layout}>
+        <div className={classes.header_img}>
+          <img src="img/img1.png" alt="" />
+        </div>
+        <div className={classes.header_text}>
+          <Title level={2} className={classes.title}>
+            AWG INSURANCE BROKERS PTE LTD
+          </Title>
+          <Title level={4} className={classes.subtitle}>
+            我们提供完善的设计原则
+          </Title>
+          <Title level={5} className={classes.paragraph}>
+            BUSINESS REGISTRATION NO: 199002234G GST NO: 199002234G 240
+            Macpherson Road #08-01, Pines Building, Singapore 348574 Tel: 65
+            6294 6688 / Fax: 65 6282 6188 / Email: awg@awginsurance.com
+          </Title>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
